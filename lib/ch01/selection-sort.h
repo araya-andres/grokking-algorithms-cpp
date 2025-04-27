c81262137c6829c634efce639a1f25fc2d4fbc07
Andres Araya <araya.andres@gmail.com>
2025-04-27 16:34:43 -0600

    use min_element on selection_sort

--- lib/ch01/selection-sort.h
+++ lib/ch01/selection-sort.h
@@ -1,28 +1,15 @@
+#include <algorithm>
 #include <vector>
-
-template <typename T>
-size_t find_smallest(const std::vector<T>& v)
-{
-    auto smallest = v[0];
-    auto smallest_index = 0;
-    for (auto i = 1; i < v.size(); i++) {
-        if (v[i] < smallest) {
-            smallest = v[i];
-            smallest_index = i;
-        }
-    }
-    return smallest_index;
-}
 
 template <typename T>
 std::vector<T> selection_sort(std::vector<T> v)
 {
     std::vector<T> retval;
     retval.reserve(v.size());
-    while (v.size() > 0) {
-        auto smallest_index = find_smallest(v);
-        retval.emplace_back(v[smallest_index]);
-        std::swap(v[smallest_index], v[v.size() - 1]);
+    while (!v.empty()) {
+        auto smallest = std::min_element(v.begin(), v.end());
+        retval.emplace_back(*smallest);
+        std::swap(*smallest, v.back());
         v.pop_back();
     }
     return retval;
