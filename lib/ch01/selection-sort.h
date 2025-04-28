e4528032bc74112a26d10b4877c569a14db8fc78
Andres Araya <araya.andres@gmail.com>
2025-04-28 06:59:12 -0600

    sort "in place"

--- lib/ch01/selection-sort.h
+++ lib/ch01/selection-sort.h
@@ -4,13 +4,9 @@
 template <typename T>
 std::vector<T> selection_sort(std::vector<T> v)
 {
-    std::vector<T> retval;
-    retval.reserve(v.size());
-    while (!v.empty()) {
-        auto smallest = std::min_element(v.begin(), v.end());
-        retval.emplace_back(*smallest);
-        std::swap(*smallest, v.back());
-        v.pop_back();
+    for (auto it = begin(v); it < end(v); it++) {
+        auto smallest = std::min_element(it, end(v));
+        std::swap(*smallest, *it);
     }
-    return retval;
+    return v;
 }
