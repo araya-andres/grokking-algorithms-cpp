0d90d88cfb73b21cc7eda25252b3be77465efaaf
Andres Araya <araya.andres@gmail.com>
2025-05-01 14:28:17 -0600

    use std::swap on quick_sort

--- lib/ch04/quicksort.h
+++ lib/ch04/quicksort.h
@@ -1,24 +1,15 @@
-#include <cstdio>
-#include <stdlib.h>
+#include <utility>
 #include <vector>
-
-template <typename T>
-void swap(T* v, int i, int j)
-{
-    auto tmp = v[i];
-    v[i] = v[j];
-    v[j] = tmp;
-}
 
 template <typename T>
 void quick_sort(T* v, int n)
 {
     if (n <= 1) return; // nothing to do
-    swap(v, 0, rand() % n);
+    std::swap(v[0], v[rand() % n]);
     auto last = 0;
     for (int i = 1; i < n; i++)
-        if (v[i] < v[0]) swap(v, ++last, i);
-    swap(v, 0, last);
+        if (v[i] < v[0]) std::swap(v[++last], v[i]);
+    std::swap(v[0], v[last]);
     quick_sort(v, last);
     quick_sort(v + last + 1, n - last - 1);
 }
